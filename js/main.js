80a0d9629063a190dbb4bc76be74a597a9438040
Saveliy <saveliyburk@gmail.com>
2021-10-02 11:35:05 +0300

    Add fish generator

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 
+const MAX_PHOTOS=25;
+const NAMES = [
+  'Савелий',
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Павел',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо.',
+  'Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+  'Как можно было поймать такой неудачный момент?!',
+];
+
+
+const getCorrectLength = (actualLength, maxLength) => {
+  const result = maxLength >= actualLength;
+  return result;
+};
 const getRandomNumber = (minValue, maxValue) => {
   const min = Math.ceil(minValue);
   const max = Math.floor(maxValue);
@@ -18,12 +49,51 @@ const getRandomNumber = (minValue, maxValue) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-getRandomNumber(10, 15);
+const MAX_COMMENTS = getRandomNumber(1, 90);
 
+const createCommentObject = (id) =>{
+  const avatar = `img/avatar-${getRandomNumber(1, 6)}.svg`;
+  const quote = MESSAGES[getRandomNumber(0, MESSAGES.length - 1)];
+  const name = NAMES[getRandomNumber(0, NAMES.length - 1)];
+  const message = getRandomNumber(1, 2) === 1 ? quote : quote + MESSAGES[getRandomNumber(0, MESSAGES.length - 1)];
 
-const getCorrectLength = (actualLength, maxLength) => {
-  const result = maxLength >= actualLength;
-  return result;
+  return {
+    id,
+    avatar,
+    message,
+    name,
+  };
+};
+const createCommentObjects = (photoId) =>{
+  const comments = [];
+  for (let id = 0; id < MAX_COMMENTS+1; id++){
+    comments.push(createCommentObject(id + String(`${photoId}`)));
+  }
+  return comments;
 };
 
+
+const createPhotoObject = (id) => {
+  const url = `photos/${id}.jpg`;
+  const likes = getRandomNumber(15,200);
+
+  return {
+    id,
+    url,
+    description:'Случайное описание фото',
+    likes:likes,
+    comments: createCommentObjects(id)};
+};
+const createPhotoObjects = () => {
+  const photos = [];
+  for (let id = 1; id < MAX_PHOTOS+1; id++) {
+    photos.push(createPhotoObject(id));
+  }
+  return photos;
+};
+
+
 getCorrectLength(150, 140);
+createPhotoObjects();
+
+
